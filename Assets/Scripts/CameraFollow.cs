1fc89c8e105a6de049d9b7216e084be81e9957d0
Justin R <u1134889@umail.utah.edu>
2020-09-26 16:25:07 -0600

    Added a scripts folder and made a simple script so the camera follows the car.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [Tooltip ("The game object which this camera should follow")]
+    public GameObject toFollow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(toFollow.transform.position.x, toFollow.transform.position.y, -10f);
+    }
+}
