24fcb76a354a3c47a25d796b8f81998e94cc148a
Justin R <u1134889@umail.utah.edu>
2020-09-30 00:47:57 -0600

    Basic UI implemented
    Added a very basic UI with speed, hp, no2, and points able to be set with the UserInterface.cs script

---
+++ Assets/Scripts/UserInterface.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserInterface : MonoBehaviour
+{
+    public static float speed = 0;
+
+    public static float hp = 100;
+
+    public static float no2 = 100;
+
+    public static int points = 0;
+
+    public GameObject speedometer;
+
+    public GameObject health;
+
+    public GameObject nitrous;
+
+    public GameObject score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Text speedt = speedometer.GetComponent<Text>();
+
+        speedt.text = "Speed: " + speed + " mph";
+
+        Text healtht = health.GetComponent<Text>();
+
+        healtht.text = "Health: " + hp + "%";
+
+        Text nitroust = nitrous.GetComponent<Text>();
+
+        nitroust.text = "NO\u2082: " + no2 + "%";
+
+        Text scoret = score.GetComponent<Text>();
+
+        scoret.text = "Score: " + no2;
+    }
+}
