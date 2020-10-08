3f3ffa70cebd6cc01da94a7aa37b1141e58e92a9
zhufeng <zzf18676456441@gmail.com>
2020-10-07 20:46:02 -0600

    apply enemy AI in level1

--- Assets/_Scripts/Enemy.cs
+++ Assets/_Scripts/Enemy.cs
@@ -17,7 +17,7 @@ public class Enemy : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if(collision.gameObject.CompareTag("Player"))
         {
             health -= collision.gameObject.GetComponent<Player>().damage;
             StartCoroutine(Wait());
