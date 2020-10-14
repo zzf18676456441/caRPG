e2eb6ba161bb3e9300c98fd208e757a0f6c6f763
MICHAEL PARKER <u0455328@umail.utah.edu>
2020-10-14 17:08:38 -0600

    Polishing touches
    Reorganized sprites into assets -> Sprites
    Added exit
    Added healthpack
    Changed stick to second-run powerup

---
+++ Assets/_Scripts/FinishLine.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishLine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameController controller;
+    void Awake()
+    {
+        controller = GameObject.Find("GameControllerObject").GetComponent<GameController>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other){
+        if (other.gameObject.tag == "Player"){
+            controller.GetPlayer().currentHealth = 400;
+            SceneManager.LoadScene(1);
+            controller.Invoke("AddStick", 1f);
+        }
+    }
+}
+
--- Assets/_Scripts/RestartHandler.cs
+++ Assets/_Scripts/RestartHandler.cs
@@ -6,13 +6,18 @@ using UnityEngine.SceneManagement;
 public class RestartHandler : MonoBehaviour
 {
     public SpriteRenderer backgroundImage;
+    GameController controller;
+    void Awake()
+    {
+        controller = GameObject.Find("GameControllerObject").GetComponent<GameController>();
+    }
     void Update()
     {
         //makes the background flash
         backgroundImage.color = new Color(.6f, .35f, .35f, Mathf.Abs(Mathf.Sin(Time.time)) + .1f);
         if (Input.anyKey)
         {
-            SceneManager.LoadScene(0);
+            SceneManager.LoadScene(1);
         }
     }
 }
