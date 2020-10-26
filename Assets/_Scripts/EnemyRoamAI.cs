ecd56caedeac5c883c8fbd2284295bc42b2555a5
zzf18676456441 <46762951+zzf18676456441@users.noreply.github.com>
2020-10-26 17:20:30 -0600

    create roaming system

---
+++ Assets/_Scripts/EnemyRoamAI.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRoamAI : MonoBehaviour
+{
+
+
+    private Vector3 startPosition;
+    private Vector3 roamPosition;
+    public bool isColide = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+        roamPosition = GetRandomPosition();
+    }
+
+    void Update()
+    {
+        GetComponent<SinglePointMovement>().MoveByVector(new Vector2(roamPosition.x, roamPosition.y));
+    }
+
+    void FixedUpdate()
+    {
+        float reachedPositionDis = 1f;
+        if((Vector3.Distance(transform.position, roamPosition) < reachedPositionDis) || isColide)
+        {
+            roamPosition = GetRandomPosition();
+            isColide = false;
+            StartCoroutine(Wait());
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            isColide = true;
+        }
+    }
+
+    private static Vector3 GetRandomDir()
+    {
+        return new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+    }
+
+    private Vector3 GetRandomPosition()
+    {
+        return startPosition + GetRandomDir() * Random.Range(10f, 70f);
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(2);
+    }
+}
--- Assets/_Scripts/Projectile.cs
+++ Assets/_Scripts/Projectile.cs
@@ -28,7 +28,7 @@ public class Projectile : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") || collision.CompareTag("Wall"))
         {
             DestroyProjectile();
         }
