2a129fb83461511356b3e7b624ef29409304a77f
donnievkubiak <donnievkubiak@gmail.com>
2020-11-25 20:14:41 -0700

    In the process of adding a boss stage with a buffed brute, added hp bars (kinda clunky implementation, maybe best if only used for boss), added new shooter shot sprite

---
+++ Assets/HPBarScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPBarScript : MonoBehaviour
+{
+    public GameObject boss;
+    public SpriteRenderer color;
+    public Transform hpbarInner;
+    private Enemy bossScript;
+    private float maxHP;
+    void Awake()
+    {
+        bossScript = boss.GetComponent<Enemy>();
+        maxHP = bossScript.health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(boss.transform.position.x, boss.transform.position.y + 6);
+        hpbarInner.localScale = new Vector3(1 * (bossScript.health / maxHP), 1);
+        color.color = new Color((.5f / (bossScript.health / maxHP)), (1f * (bossScript.health / maxHP)), .1f);
+    }
+}
