e72b7b9ab9aeeca48fcce99618c1466fd5ac4690
donnievkubiak <donnievkubiak@gmail.com>
2020-11-30 23:10:18 -0700

    Added SFX to changing upgrades

--- Assets/_Scripts/Garage/GarageItemButton.cs
+++ Assets/_Scripts/Garage/GarageItemButton.cs
@@ -3,18 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class GarageItemButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class GarageItemButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     public GarageSlider slider;
     public GameObject item;
 
-    public void OnPointerEnter(PointerEventData eventData){
+    private AudioSource source;
+
+    void Awake()
+    {
+        source = FindObjectOfType<Camera>().GetComponent<AudioSource>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
         Debug.Log("THIS IS HAPPENING!!!");
         slider.ShowPotentialItem(item);
     }
 
-    public void OnPointerExit(PointerEventData eventData){
+    public void OnPointerExit(PointerEventData eventData)
+    {
         slider.SetSlidersOnNewEquip();
     }
 
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        source.pitch = UnityEngine.Random.Range(.95f, 1.15f);
+        source.Play();
+    }
 }
