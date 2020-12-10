00208fc88713ef7aeb40f22de8f69547f765c421
MICHAEL PARKER <u0455328@umail.utah.edu>
2020-12-10 14:58:25 -0700

    Level 4 updates

---
+++ Assets/CarPaint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarPaint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = new Color(Random.value, Random.value, Random.value, 1f);
+        gameObject.GetComponent<Rigidbody2D>().rotation += -90 + 180 * Random.value;
+    }
+
+}
